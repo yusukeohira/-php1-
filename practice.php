9eeba4dd208912645edc56f7b365d38d8f483e34
”Yusuke <circle2006yu@yahoo.co.jp>
2019-07-07 13:03:09 +0000

    課題提出php

---
+++ practice.php
@@ -0,0 +1,18 @@
+<?php
+echo 'hello php!';
+$a=3;
+$b=7;
+
+echo $a+$b;
+$array=["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array[7];
+
+$hello="Hello,";
+$name="Ohira Yusuke";
+$world="'s World";
+echo $hello,$name,$world;
+
+$tech_boost="tech";
+$tech_boost .=" boost";
+echo $tech_boost;
+
