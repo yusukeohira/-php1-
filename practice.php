738137f0ffb07211465637bf91800a9b317cf3ef
”Yusuke <circle2006yu@yahoo.co.jp>
2019-07-07 13:54:33 +0000

    課題提出php

--- practice.php
+++ practice.php
@@ -16,3 +16,18 @@ $tech_boost="tech";
 $tech_boost .=" boost";
 echo $tech_boost;
 
+$calendar_2018 = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+echo $calendar_2018["December"];
